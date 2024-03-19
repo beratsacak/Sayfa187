50ccfeaf6beaf335370d96b3fdda3f7e0ddf5e67
berat584 <berat584@BPC08>
2024-03-19 11:36:47 +0300

    Proje dosyası ekle.

---
+++ Sayfa187/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Sayfa187
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numara = new System.Windows.Forms.TextBox();
+            this.adSoyad = new System.Windows.Forms.TextBox();
+            this.alan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Numara";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Alan";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bagla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numara
+            // 
+            this.numara.Location = new System.Drawing.Point(267, 87);
+            this.numara.Name = "numara";
+            this.numara.Size = new System.Drawing.Size(100, 20);
+            this.numara.TabIndex = 2;
+            // 
+            // adSoyad
+            // 
+            this.adSoyad.Location = new System.Drawing.Point(267, 113);
+            this.adSoyad.Name = "adSoyad";
+            this.adSoyad.Size = new System.Drawing.Size(100, 20);
+            this.adSoyad.TabIndex = 2;
+            this.adSoyad.TextChanged += new System.EventHandler(this.adSoyad_TextChanged);
+            // 
+            // alan
+            // 
+            this.alan.Location = new System.Drawing.Point(267, 139);
+            this.alan.Name = "alan";
+            this.alan.Size = new System.Drawing.Size(100, 20);
+            this.alan.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.alan);
+            this.Controls.Add(this.adSoyad);
+            this.Controls.Add(this.numara);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox numara;
+        private System.Windows.Forms.TextBox adSoyad;
+        private System.Windows.Forms.TextBox alan;
+    }
+}
+
---
+++ Sayfa187/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayfa187
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        class Ogrenciler
+        {
+            public int Numara { get; set; }
+            public string AdSoyad { get; set; }
+            public string Alan { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenciler ogrenci = new Ogrenciler();
+            ogrenci.Numara = 17;
+            ogrenci.AdSoyad = "selo";
+            ogrenci.Alan = "Bilişim Teknolojileri";
+            numara.DataBindings.Add("Text", ogrenci, "Numara");
+            adSoyad.DataBindings.Add("Text", ogrenci, "AdSoyad");
+            alan.DataBindings.Add("Text", ogrenci, "Alan");
+        }
+
+        private void adSoyad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
